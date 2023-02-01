ec258634beab38cca4eae81bf4df96c79c491947
then0403 <39440926+then0403@users.noreply.github.com>
2023-02-01 14:29:45 -0800

    finished build

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import SideBar from '../components/SideBar';
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return ( 
+    <SideBar>
+    <Component {...pageProps} />
+    </SideBar>
+  )
 }
